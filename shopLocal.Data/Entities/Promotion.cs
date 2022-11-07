2b884c2d2ad4394853e6c395de093afc33c91aff
Tuanh <tuanh.2510.ccpr@gmail.com>
2022-11-08 00:22:58 +0700

    Database design full

---
+++ shopLocal.Data/Entities/Promotion.cs
@@ -0,0 +1,22 @@
+﻿using shopLocal.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopLocal.Data.Entities
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool ApplyForAll { get; set; }
+        public int ? DiscountAmount { get; set; }
+        public string ProductIds { get; set; }
+        public string ProductCategoryIds { get; set; }
+        public Status Status { get; set; }
+        public string Name { get; set; }
+    }
+}
