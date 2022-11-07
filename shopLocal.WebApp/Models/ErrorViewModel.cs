5c355e9838308c5297314cb4730f7e893207cfd9
Tuanh <tuanh.2510.ccpr@gmail.com>
2022-11-07 15:53:39 +0700

    Add project files.

---
+++ shopLocal.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shopLocal.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
