4c925cec4a00b650f494c788e5f9235c1af9a8c9
Tuanh <tuanh.2510.ccpr@gmail.com>
2022-11-07 17:48:16 +0700

    Desgin Database Product and Category

---
+++ shopLocal.Data/EF/shopLocationDdContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shopLocal.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopLocal.Data.EF
+{
+    public class shopLocationDdContext : DbContext
+    {
+        public shopLocationDdContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+        public DbSet<Product> Products ;
+        public DbSet<Category> Categories ;
+    }
+}
