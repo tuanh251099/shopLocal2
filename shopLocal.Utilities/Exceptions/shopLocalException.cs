b4dfdbfe5716314b771d9bd88f14739b718bcfec
Tuanh <tuanh.2510.ccpr@gmail.com>
2022-11-11 15:26:21 +0700

    update datalayer 3

---
+++ shopLocal.Utilities/Exceptions/shopLocalException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopLocal.Utilities.Exxeptions
+{
+    public class shopLocalException : Exception
+    {
+        public shopLocalException()
+        {
+
+        }
+        public shopLocalException(string message): base(message)
+        {
+
+        }
+        public shopLocalException(string message, Exception inner): base(message, inner)
+        {
+
+        }
+    }
+}
