42c84f46c0f71b1667150f75856ff5f729a2edf6
Tuanh <tuanh.2510.ccpr@gmail.com>
2022-11-15 17:55:49 +0700

    Create Restful Api

--- shopLocal.BackendAPI/Controllers/ProductController.cs
+++ shopLocal.BackendAPI/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using shopLocal.Application.Catalog.Products;
+using shopLocal.ViewModels.Catalog.Products.Manage;
 
 namespace shopLocal.BackendAPI.Controllers
 {
@@ -9,15 +10,71 @@ namespace shopLocal.BackendAPI.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
-        public ProductController(IPublicProductService publicProductService)
+        private readonly IManageProductService _manageProductService;
+        public ProductController(IPublicProductService publicProductService, IManageProductService manageProductService)
         {
             _publicProductService = publicProductService;
+            _manageProductService = manageProductService;
         }
+
+        //http://localhost:port/product
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             var products = await _publicProductService.GetAll();
             return Ok(products);
+        }
+
+        //http://localhost:port/product/public-paging
+        [HttpGet("public-paging")]
+        public async Task<IActionResult> Get([FromQuery] GetPublicProductPagingRequest request)
+        {
+            var products = await _publicProductService.GetAllByCategoryId(request);
+            return Ok(products);
+        }
+
+        //http://localhost:port/product/id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _manageProductService.GetById(id);
+            if (product == null)
+                return BadRequest("Can not find product");
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]ProductCreateRequest request)
+        {
+            var productId = await _manageProductService.Create(request);
+            if (productId == 0)
+                return BadRequest();
+            var product = await _manageProductService.GetById(productId);
+            return CreatedAtAction(nameof(GetById),new {id = productId} ,product);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] ProductUpdateRequest request)
+        {
+            var affectedResult = await _manageProductService.Update(request);
+            if (affectedResult == 0)
+                return BadRequest();
+            return Ok();
+        }
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var affectedResult = await _manageProductService.Delete(productId);
+            if (affectedResult == 0)
+                return BadRequest();
+            return Ok();
+        }
+        [HttpPut("price/{id}/{newPrice}")]
+        public async Task<IActionResult> UpdatePrice([FromQuery] int id, decimal newPrice)
+        {
+            var isSuccessful = await _manageProductService.UpdatePrice(id, newPrice);
+            if (isSuccessful)
+                return Ok();
+            return BadRequest();
         }
     }
 }
