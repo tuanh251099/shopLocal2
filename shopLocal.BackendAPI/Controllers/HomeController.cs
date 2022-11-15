c4e498359431f80cfd78423eb264374c7b863923
Tuanh <tuanh.2510.ccpr@gmail.com>
2022-11-15 15:16:35 +0700

    Create WebBackEnd Api

---
+++ shopLocal.BackendAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shopLocal.BackendAPI.Models;
+using System.Diagnostics;
+
+namespace shopLocal.BackendAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ shopLocal.Utilitiesfix/Constants/SystemContants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopLocal.Utilities.Constants
+{
+    public class SystemContants
+    {
+        public const string MainConnectionString = "shopLocation";
+    }
+}
